ea913e8e9f4aa12c50afba7a308d1a67e928ddb3
Your Name <akashmondkar03@gmail.com>
2026-05-21 23:15:48 +0530

    about page

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-import express from "express";
-import home from "./pages/home.js";
-import login from "./pages/login.js";
-import submit from "./pages/submit.js";
-const app = express();
+import express from 'express'
 
-app.get("/", (req, resp) => {
-  resp.send(home());
-});
+const app =express();
 
-app.get("/login", (req, resp) => {
-  resp.send(login());
-});
 
-app.post("/submit", (req, resp) => {
-  resp.send(submit());
-});
-
-app.listen(3807);
+app.listen(3860)
