176e1e8e4c2b339233c282865b14769c0f8a23d1
Your Name <akashmondkar03@gmail.com>
2026-05-21 20:26:29 +0530

    module

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const express = require('express');
+// const express = require('express');
+
+import express from 'express'
 
 const app = express();
 
 
-app.listen(3400)
+app.get("",(req,resp)=>{
+   resp.send("<h1>Home page</h1>")
+})
+
+
+app.get("/about",(req,resp)=>{
+   resp.send("<h1>about page</h1>")
+})
+
+
+
+
+
+app.listen(3400);
