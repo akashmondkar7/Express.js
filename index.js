9050c18d72bc27076fd2520198d4279593f017dd
Your Name <akashmondkar03@gmail.com>
2026-05-22 00:01:44 +0530

    fix

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 import express from 'express';
 import path from 'path';
-const app =express();
 
-const publicPath=path.resolve('public')
-app.use(express.static(publicPath))
-app.get("/",(res,resp)=>{
- const abspath=path.resolve("view/home.html")
-  resp.sendFile(abspath)
-})
+const app = express();
 
-app.get("/login",(res,resp)=>{
- const abspath=path.resolve("view/login.html")
-  resp.sendFile(abspath)
-})
+const publicPath = path.resolve('public');
 
-app.get("/about",(res,resp)=>{
- const abspath=path.resolve("view/about.html")
-  resp.sendFile(abspath)
-})
+app.use(express.static(publicPath));
 
+app.get("/", (req, resp) => {
+  const abspath = path.resolve("view/home.html");
+  resp.sendFile(abspath);
+});
 
-app.listen(3860)
+app.get("/login", (req, resp) => {
+  const abspath = path.resolve("view/login.html");
+  resp.sendFile(abspath);
+});
+
+app.get("/about", (req, resp) => {
+  const abspath = path.resolve("view/about.html");
+  resp.sendFile(abspath);
+});
+
+app.listen(3860, () => {
+  console.log("Server running on port 3860");
+});
