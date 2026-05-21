522ee964d0c981e32250a68bb87bf854f41bd59a
Your Name <akashmondkar03@gmail.com>
2026-05-21 23:16:25 +0530

    import html file

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-import express from 'express'
-
+import express from 'express';
+import path from 'path';
 const app =express();
+
+app.get("/",(res,resp)=>{
+ const abspath=path.resolve("view/home.html")
+  resp.sendFile(abspath)
+})
+
+app.get("/login",(res,resp)=>{
+ const abspath=path.resolve("view/login.html")
+  resp.sendFile(abspath)
+})
+
+app.get("/about",(res,resp)=>{
+ const abspath=path.resolve("view/about.html")
+  resp.sendFile(abspath)
+})
 
 
 app.listen(3860)
