900367a1747852ad0d1e5dacdf52719f189e0c49
Your Name <akashmondkar03@gmail.com>
2026-05-22 22:27:02 +0530

    pages

--- index.js
+++ index.js
@@ -6,4 +6,21 @@ import express from 'express'
 
   })
 
+  app.get('/user',(res,resp)=>{
+    resp.send("User")
+
+
+  })
+
+  app.get('/product',(res,resp)=>{
+    resp.send("Product")
+
+
+  })
+
+  app.get('/about',(res,resp)=>{
+    resp.send("about")
+
+
+  })
   app.listen(3806)
