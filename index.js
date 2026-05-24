6287079452fc0f13392c18bd5c57fadfabfd9959
Your Name <akashmondkar03@gmail.com>
2026-05-24 16:07:05 +0530

    external middleware

--- index.js
+++ index.js
@@ -1,53 +1,21 @@
 import express from 'express'
-const app = express();
+import morgan from 'morgan'
+const app= express();
 
-
-
-function checkAgeRouteMiddleware(req,resp,next){
-  if(!req.query.age || req.query.age<18){
-    resp.send("you are not allowed to use this page")
-  }else{
-    next();
-  }
-}
-
-function checkURLRouteMiddleware(req,resp,next){
-  console.log("this request url is",req.url);
-
-  next();
-}
-
-
-
-
-app.get('',(rep,resp)=>{
-
-  resp.send("<h1>Home Page</h1>")
-
+app.use(morgan('dev'))
+app.get("/",(req,resp)=>{
+  resp.send("Home page ")
 })
 
-app.get('/login',checkURLRouteMiddleware,(rep,resp)=>{
 
-  resp.send("<h1>login Page</h1>")
-
+app.get("/users",(req,resp)=>{
+  resp.send("users page ")
 })
 
-app.get('/admin',(rep,resp)=>{
-
-  resp.send("<h1>Admin Page</h1>")
-
+app.get("/wait",(req,resp)=>{
+  setTimeout(()=>{
+    resp.send("result after 1 sec")
+  },1000);
 })
 
-app.get('/user',checkAgeRouteMiddleware,checkURLRouteMiddleware,(rep,resp)=>{
-
-  resp.send("<h1>User Page</h1>")
-
-})
-
-app.get('/products',checkAgeRouteMiddleware,checkURLRouteMiddleware,(rep,resp)=>{
-
-  resp.send("<h1>products Page</h1>")
-
-})
-
-app.listen(3200)
+app.listen(4800)
