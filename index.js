0196c9f1aaad7ccfaedc7cf5c91fd8a84353650a
Your Name <akashmondkar03@gmail.com>
2026-05-24 11:08:02 +0530

    save

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 import express from 'express'
 const app = express();
 
+
+function ageCheck(req,resp,next){
+  if(!req.query.age || req.query.age<18){
+      resp.send("you can not acces this page")
+  }
+}
+
  app.get("/",(req,resp)=>{
   resp.send("home page")
+})
 
-  
- }).listen(4800)
+ app.get("/admin",(req,resp)=>{
+  resp.send("about page")
+})
+
+ app.get("/user",(req,resp)=>{
+  resp.send("user page")
+})
+
+ app.get("/login",(req,resp)=>{
+  resp.send("login page")
+}).listen(4800)
