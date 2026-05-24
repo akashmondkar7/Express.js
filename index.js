5d6c9c646924e21f6e676e96ff5f026542834e4d
Your Name <akashmondkar03@gmail.com>
2026-05-24 12:59:28 +0530

    done route middleware

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ import express from 'express'
 const app = express();
 
 
+
+function checkAgeRouteMiddleware(req,resp,next){
+  if(!req.query.age || req.query.age<18){
+    resp.send("you are not allowed to use this page")
+  }else{
+    next();
+  }
+}
+
+function checkURLRouteMiddleware(req,resp,next){
+  console.log("this request url is",req.url);
+
+  next();
+}
+
+
+
+
 app.get('',(rep,resp)=>{
 
   resp.send("<h1>Home Page</h1>")
 
 })
 
-app.get('/login',(rep,resp)=>{
+app.get('/login',checkURLRouteMiddleware,(rep,resp)=>{
 
   resp.send("<h1>login Page</h1>")
 
@@ -20,13 +38,13 @@ app.get('/admin',(rep,resp)=>{
 
 })
 
-app.get('/user',(rep,resp)=>{
+app.get('/user',checkAgeRouteMiddleware,checkURLRouteMiddleware,(rep,resp)=>{
 
   resp.send("<h1>User Page</h1>")
 
 })
 
-app.get('/products',(rep,resp)=>{
+app.get('/products',checkAgeRouteMiddleware,checkURLRouteMiddleware,(rep,resp)=>{
 
   resp.send("<h1>products Page</h1>")
 
