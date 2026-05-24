387f2dd7834b232a27191383110995dc8dbc3593
Your Name <akashmondkar03@gmail.com>
2026-05-24 17:04:52 +0530

    ejs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express'
-import morgan from 'morgan'
+// import morgan from 'morgan'
 const app= express();
 
-app.use(morgan('dev'))
+// app.use(morgan('dev'))
 app.get("/",(req,resp)=>{
   resp.send("Home page ")
 })
@@ -12,10 +12,19 @@ app.get("/users",(req,resp)=>{
   resp.send("users page ")
 })
 
-app.get("/wait",(req,resp)=>{
-  setTimeout(()=>{
-    resp.send("result after 1 sec")
-  },1000);
+app.get("/errors",(req,resp)=>{
+ const error = new Error('')
+ error.status=404;
+ next(error);
 })
 
+// app.get("/wait",(req,resp)=>{
+//   setTimeout(()=>{
+//     resp.send("result after 1 sec")
+//   },1000);
+// })
+// function errorsHandling
+app.use((error,req,resp,next)=>{
+  resp.status(error.status || 500).send("Try after some time")
+})
 app.listen(4800)
