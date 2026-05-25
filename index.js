f1c4a6c058c984cf686751ca100bfd293b7adff9
Your Name <akashmondkar03@gmail.com>
2026-05-25 21:23:49 +0530

    mongodb

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
 import express from 'express'
-import userData from './user.json' with{type:'json'}
-// import { name } from 'ejs';
-const app=express();
+import { MongoClient } from 'mongodb';
 
 
-app.get("/",(req,resp)=>{
- 
-  resp.send(userData)
-})
 
-app.get("/user/:id",(req,resp)=>{
-  const id = req.params.id
-  console.log(id);
-  let filteredData = userData.filter((user)=>user.id==id)
-    resp.send(filteredData)
 
-})
 
-app.get("/username/:name",(req,resp)=>{
-  const name= req.params.name
-  console.log(name);
-  let filteredData = userData.filter((user)=>user.name.toLowerCase()==name.toLowerCase())
-    resp.send(filteredData)
+const app= express();
 
-})
-app.listen(3210);
+
